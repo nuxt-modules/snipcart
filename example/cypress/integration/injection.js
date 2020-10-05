94f9ec171057863295cdceb88ea79de4796edb20
f3ltron <hello@florent.dev>
2020-10-05 19:09:03 -0400

    feat: adding add product behavior + cypress test + update example

--- example/cypress/integration/injection.js
+++ example/cypress/integration/injection.js
@@ -4,12 +4,6 @@ describe('Snipcart', () => {
   it('snipcart element should have been injected', () => {
     cy.visit('/')
 
-     cy.server();
-     cy.route({
-       method: "GET",
-       url: "https://cdn.snipcart.com/**"
-     }).as("apiCheck");
-
     cy.get('html').then((el) => {
       const html = el[0].innerHTML
 
@@ -32,7 +26,6 @@ describe('Snipcart', () => {
       );
     })
 
-    cy.wait("@apiCheck");
     cy.wait(1000)
 
     cy.get('html').then((el) => {
